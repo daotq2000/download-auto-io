caabcf574fd988c38b07c2d81f108fc07f0244eb
daotq1 <v.daotq1@vinhomes.vn>
2025-01-13 11:37:45 +0700

    initialize

---
+++ js/common.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$.get("../header.html",(function(data,status){console.log(data),$("#masthead").html(data)})),$.get("../footer.html",(function(data,status){console.log(data),$("#contact-us").html(data)}))}));
